0ecd70fff9791795f0a2ddc854dd87ac8dadd379
jmvillafanamdoza <144737939+jmvillafanamdoza@users.noreply.github.com>
2023-11-15 18:29:37 -0500

    repository

---
+++ ApiConfigService/src/main/java/pe/cibertec/ecommerce/ApiConfigService/ApiConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package pe.cibertec.ecommerce.ApiConfigService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ApiConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigServiceApplication.class, args);
+	}
+
+}
---
+++ ApiConfigService/src/test/java/pe/cibertec/ecommerce/ApiConfigService/ApiConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.ecommerce.ApiConfigService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
